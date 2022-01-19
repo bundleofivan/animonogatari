d8974ad6a41feaa33986cbb1f58000336571944e
Ivan Lim <bundleofivan@gmail.com>
2022-01-19 14:14:20 -0800

    added auth login reducer skeleton

--- src/store/actions/auth.ts
+++ src/store/actions/auth.ts
@@ -1,19 +1,27 @@
-import { AppDispatch } from 'store'
+import axios from 'axios'
+
+import { AppDispatch, RootState } from 'store'
 
 export const login = () => {
-    return async (dispatch: AppDispatch) => {
-        dispatch({type: 'TEST', payload: {}})
+    return async (dispatch: AppDispatch, getState: () => RootState) => {
+        dispatch({type: 'AUTH_LOGIN', payload: {}})
+        const { auth: { inputUsername, inputPassword }} = getState()
+        try {
+            dispatch({type: 'AUTH_LOGIN_SUCCESS', payload: {}})
+        } catch {
+            dispatch({type: 'AUTH_LOGIN_FAILURE', payload: {}})
+        }
     }
 }
 
 export const setUsername = (newInput: string) => {
     return (dispatch: AppDispatch) => {
-        dispatch({type: 'SET_USERNAME', payload: { inputUsername: newInput }})
+        dispatch({type: 'AUTH_SET_USERNAME', payload: { inputUsername: newInput }})
     }
 }
 
 export const setPassword = (newInput: string) => {
     return (dispatch: AppDispatch) => {
-        dispatch({type: 'SET_PASSWORD', payload: { inputPassword: newInput }})
+        dispatch({type: 'AUTH_SET_PASSWORD', payload: { inputPassword: newInput }})
     }
 }
--- src/store/reducers/auth.ts
+++ src/store/reducers/auth.ts
@@ -3,24 +3,47 @@ import createReducer from 'store/createReducer'
 export type AuthState = {
     inputUsername: string
     inputPassword: string
+    authenticating: boolean
 }
 
 const initialState: AuthState = {
     inputUsername: '',
-    inputPassword: ''
+    inputPassword: '',
+    authenticating: false,
 }
 
 const auth = createReducer(initialState, {
-    SET_USERNAME: <S>(state: S, payload: Partial<AuthState>) => {
+    AUTH_SET_USERNAME: <S>(state: S, payload: Partial<AuthState>) => {
         return {
             ...state,
             ...payload,
         }
     },
-    SET_PASSWORD: <S>(state: S, payload: Partial<AuthState>) => {
+    AUTH_SET_PASSWORD: <S>(state: S, payload: Partial<AuthState>) => {
         return {
             ...state,
             ...payload,
+        }
+    },
+    AUTH_LOGIN: <S>(state: S, payload: Partial<AuthState>) => {
+        return {
+            ...state,
+            ...payload,
+            authenticating: true
+        }
+    },
+    AUTH_LOGIN_SUCCESS: <S>(state: S, payload: Partial<AuthState>) => {
+        return {
+            ...state,
+            ...payload,
+            ...initialState,
+        }
+    },
+    AUTH_LOGIN_FAILURE: <S>(state: S, payload: Partial<AuthState>) => {
+        return {
+            ...state,
+            ...payload,
+            authenticating: false
         }
     }
 })
