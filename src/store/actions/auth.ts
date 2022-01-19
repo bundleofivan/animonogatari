f59ee450267276bdf1aad074ac33769621475c2b
Ivan Lim <bundleofivan@gmail.com>
2022-01-19 14:35:15 -0800

    pasted in auth url with axios GET but need to setup middleware server to work around CORS

--- src/store/actions/auth.ts
+++ src/store/actions/auth.ts
@@ -5,9 +5,13 @@ import { AppDispatch, RootState } from 'store'
 export const login = () => {
     return async (dispatch: AppDispatch, getState: () => RootState) => {
         dispatch({type: 'AUTH_LOGIN', payload: {}})
-        const { auth: { inputUsername, inputPassword }} = getState()
         try {
+            const { auth: { inputUsername, inputPassword }} = getState()
+            const authUrl = 'https://myanimelist.net/v1/oauth2/authorize'
+            // const res = await axios.get(authUrl)
+
             dispatch({type: 'AUTH_LOGIN_SUCCESS', payload: {}})
+
         } catch {
             dispatch({type: 'AUTH_LOGIN_FAILURE', payload: {}})
         }
